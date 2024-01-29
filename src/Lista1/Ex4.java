5fd8fb024b7375c56cd4fb6fa36b316b5227f595
LuxsterX <123011704+LuxsterX@users.noreply.github.com>
2024-01-29 12:45:27 +0100

    Ex4 fix (corrected list references) commit

--- src/Lista1/Ex4.java
+++ src/Lista1/Ex4.java
@@ -9,21 +9,23 @@ public class Ex4 {
         System.out.print("Podaj liczbę całkowitą z przedziału [1,9]: ");
         int n = scanner.nextInt();
 
-        scanner.close();
+
 
         if ((n > 9) || (n < 1)) {
             System.out.print("Podano liczbę spoza dopuszczalnego przedziału.");
         } else {
             int[][] tabA = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    tabA[i][j] = i * j;
+            for (int i = 0; i <= (n-1); i++) {
+                for (int j = 0; j <= (n-1); j++) {
+                    tabA[i][j] = (i+1) * (j+1);
                     System.out.print(tabA[i][j] + "\t");
                 }
                 System.out.println();
             }
 
         }
+
+        scanner.close();
     }
 }
 
